28e3b565219ab6105cb56d84f107697d35316418
Daniel Cubo <danielcubo@gmail.com>
2024-11-16 09:49:22 -0300

    Project Created

---
+++ src/main/java/com/devsuperior/gamestream/GamestreamApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.gamestream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamestreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamestreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/gamestream/GamestreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.gamestream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamestreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
